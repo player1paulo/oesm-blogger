7c5ddcb8426e1328f7cae1a7aa2a43d2f634e604
Paulo Chaparro <108769238+player1paulo@users.noreply.github.com>
2023-10-01 18:11:05 +0100

    vários melhoramentos

--- oesm.js
+++ oesm.js
@@ -17,3 +17,18 @@ document.addEventListener("DOMContentLoaded", function() {
 		document.querySelector(".progress--bar").style.width = percentage + "%";
 	}
 });
+
+function copylinktoclipboard() {
+	// Get the text field
+	var copyText = document.getElementById("myInput");
+  
+	// Select the text field
+	copyText.select();
+	copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+	 // Copy the text inside the text field
+	navigator.clipboard.writeText(copyText.value);
+  
+	// Alert the copied text
+	alert("Copied the text: " + copyText.value);
+  }
