7a0fddb73b02ee27d317f5b0fc7fa57026a4a41d
Paulo Chaparro <108769238+player1paulo@users.noreply.github.com>
2023-10-03 14:55:01 +0100

    melhoramentos css viewport mobile

--- oesm.js
+++ oesm.js
@@ -17,18 +17,3 @@ document.addEventListener("DOMContentLoaded", function() {
 		document.querySelector(".progress--bar").style.width = percentage + "%";
 	}
 });
-
-function copylinktoclipboard() {
-	// Get the text field
-	var copyText = document.getElementById("myInput");
-  
-	// Select the text field
-	copyText.select();
-	copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-	 // Copy the text inside the text field
-	navigator.clipboard.writeText(copyText.value);
-  
-	// Alert the copied text
-	alert("O link foi copiado" + copyText.value);
-  }
